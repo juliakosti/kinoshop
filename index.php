19846e6ccac9d2ef739d954792bdf2e8f9e16687
juliakosti <juliakos83@gmail.com>
2019-02-25 21:35:54 +0300

    случайное

--- index.php
+++ index.php
@@ -8,11 +8,15 @@ require_once 'shop/basereq_class.php';
 $products = new Products();
 //echo $products->db->dbname;
 echo '<br/>';
-//$products->GetAllProducts();
-//print_r($products->sel);
-echo '<br/>';
-$products->GetProductByID('2');
+$products->getAllNewProd();
+//$products->GetProductsBySectionID(5);
+echo '<pre>';
+print_r($products->ids);
 print_r($products->sel);
+echo '</pre>';
+echo '<br/>';
+//$products->GetProductByID('2');
+//print_r($products->sel);
 
 
 //$dbj = new BaseConnect(KS_BASE, KS_BASE_US, KS_BASE_PASS);
