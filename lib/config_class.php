547a4e664ad02bcec176fc653a3f64299122c100
juliakosti <juliakos83@gmail.com>
2019-03-20 11:17:04 +0300

    Create config_class.php
    добавила общий класс

---
+++ lib/config_class.php
@@ -0,0 +1,35 @@
+<?php
+class Config {
+	//public $secret = "DFSJLFSDLJG";
+	public $sitename = "kinoshop.ru";
+	public $address = "http://kinoshop/";
+	//public $address_admin = "http://shopdvd.local/admin/";
+	public $db_host = "localhost";
+	public $db_user = "root";
+	public $db_password = "";
+	public $db_name = "kinoshop";
+	public $db_prefix = "sdvd_";
+	//public $sym_query = "{?}";
+	
+	public $admname = "веб-студия АлМиТи";
+	public $admemail = "almiti@inbox.ru";
+	//public $adm_login = "Jul";
+	//public $adm_password = "";
+	
+	//public $count_on_page = 8;
+	//public $count_others = 6;
+	
+	//public $pagination_count = 10;
+	
+	//public $dir_text = "lib/text/";
+	//public $dir_tmpl = "tmpl/";
+	//public $dir_tmpl_admin = "admin/tmpl/";
+	//public $dir_img_products = "images/products/";
+	
+	//public $max_name = 255;
+	//public $max_title = 255;
+	//public $max_text = 65535;
+	
+	// public $max_size_img = 102400;
+	
+}
