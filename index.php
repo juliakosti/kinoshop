2a075f171bc6c3aa6579e2b5abdacacfa0c276b2
juliakosti <juliakos83@gmail.com>
2019-03-20 11:17:14 +0300

    Update index.php
    вспом

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
-require_once 'start_php';
+require_once 'start.php';
 
 require_once $dir_lib.'url_class.php';
 
 $url = new URL();
+
+//метод определяет название данного класса
+//$view = $url->getView();
+
+$uri = $_SERVER['REQUEST_URI'];
+echo $uri;
+echo '</br>';
+echo $url->getThisURL();
+echo '</br>';
+echo $url->cart();
