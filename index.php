4db0fb77d17fbf31fd96a72eb1e8b1fe5cc7a10c
juliakosti <juliakos83@gmail.com>
2019-03-22 22:19:23 +0300

    вспом

--- index.php
+++ index.php
@@ -2,16 +2,6 @@
 
 require_once 'start.php';
 
-require_once $dir_lib.'url_class.php';
+require_once 'lib/page_class.php';
 
-$url = new URL();
-
-//метод определяет название данного класса
-//$view = $url->getView();
-
-$uri = $_SERVER['REQUEST_URI'];
-echo $uri;
-echo '</br>';
-echo $url->getThisURL();
-echo '</br>';
-echo $url->cart();
+$mainpage = new Page;
