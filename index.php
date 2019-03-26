0235a72fa961bdcfd4a97cc05ee71ca97a618eb2
juliakosti <juliakos83@gmail.com>
2019-03-26 15:10:07 +0300

    трансформация эт1

--- index.php
+++ index.php
@@ -4,4 +4,89 @@ require_once 'start.php';
 
 require_once 'lib/page_class.php';
 
-$mainpage = new Page;
+$page = new Page;
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<?$page->getMetategs();?>
+	<!--metatags.tpl-->
+</head>
+<body>
+<!--Главная обёртка -->
+<div class="wrap">
+<!--Шапка -->
+	<header>
+		<?$page->getHat();?>
+		<!--hat.tpl -->
+		
+	</header>
+	<!--/Шапка -->
+
+	<!--Меню и поиск -->
+	<?$page->getTopmenu();?>
+	
+	<!--/Меню и поиск -->
+
+	<!--Боковая панель -->
+		<?$page->getLeftmenu();?>
+		<!--leftmenu.tpl -->
+		<div class="start"></div>
+		
+	<!--/Боковая панель -->
+	<!--Основной контент -->
+	<div class="content ">
+		<?
+		$page->TransformNews();
+		$page->getContent();?>
+		<!--content.tpl -->
+				
+	</div>
+	<div class="clear"></div>
+	<!--/Основной контент -->
+	<!--Оплата -->
+	<div class="payment">
+			<p>Способы оплаты: </p>
+			<a href="#"></a>
+			<a href="#"></a>
+			<a href="#"></a>
+			<a href="#"></a>
+			<a href="#"></a>
+	</div>
+	<!--/Оплата -->
+	<!--Подвал -->
+	
+	<footer><!--footer.tpl -->
+		<?$page->getFooter();?>
+	</footer>
+	<!--/Подвал -->
+</div>
+<!--/Главная обёртка -->
+	
+<!-- Поиск -->
+<script>
+var checkInput = function(input) {
+	if (input.value.length > 0) {
+		input.className = 'active_form';
+	} else {
+		input.className = '';
+	}
+};
+
+</script>
+<!--Разделы в мобильном -->
+<script>
+$(".start").click(function(){
+ $('.sections').toggleClass("action");
+ 
+});
+$(".stop").click(function(){
+ $('.sections').toggleClass("action");
+ 
+});
+
+</script> 
+</body>
+</html>
--- lib/page_class.php
+++ lib/page_class.php
@@ -11,6 +11,7 @@ class Page
 	private $products;
 	private $sections;
 	private $url;
+	public $x;
 
 	public function __construct()
 	{
@@ -20,8 +21,42 @@ class Page
 		$this->url = new URL;
 
 		$this->secArray = $this->sections->getAllSections();
-		$this->newsArray = $this->products->getNewProd();
+		
 		
 
+	}
+	
+	public function TransformNews()
+	{   
+		$this->newsArray = $this->products->getNewProd();
+		foreach ($this->newsArray as $key => $value) {
+			$this->newsArray[$key]['img'] = $this->config->dirProdImg.$this->newsArray[$key]['img'];
+		}
+		return $this->newsArray;
+	}
+
+	public function getMetategs()
+	{
+		include_once 'tmpl/metategs.tpl';
+	}
+	public function getHat()
+	{
+		include_once 'tmpl/hat.tpl';
+	}
+	public function getTopmenu()
+	{
+		include_once 'tmpl/topmenu+search.tpl';
+	}
+	public function getLeftmenu()
+	{
+		include_once 'tmpl/leftmenu.tpl';
+	}
+	public function getContent()
+	{
+		include_once 'tmpl/content.tpl';
+	}
+	public function getFooter()
+	{
+		include_once 'tmpl/footer.tpl';
 	}
 }
